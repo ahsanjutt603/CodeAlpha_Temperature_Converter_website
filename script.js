313b5afa9a390c278828c6406c49660f58620e2e
Muhammad Ahsan Farooq <ahsanfarooqnust@gmail.com>
2023-09-12 09:41:27 +0500

    third commit

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ function Temperature_Convert() {
     let convertedTempResult = document.getElementById('convertedTempResult');
     let convertedTemp = '';
 
-    console.log(inputTemperature)
     if (inputTemperature == NaN) {
         return convertedTempResult.textContent = `Please Enter the temperature`
     } 
 
     if (inputScale === 'C' && convertScale === 'F') {
         convertedTemp = ((inputTemperature * 9 / 5) + 32).toFixed(1);
+        convertedTempResult.textContent = `Converted Temperature is: ${convertedTemp}\u00B0${convertScale}`
     } 
 
     else if (inputScale === 'F' && convertScale === 'C') {
         convertedTemp = ((inputTemperature - 32) * 5 / 9).toFixed(1);
+        convertedTempResult.textContent = `Converted Temperature is: ${convertedTemp}\u00B0${convertScale}`
     } 
     
     else if (inputScale === 'C' && convertScale === 'C') {
-        convertedTemp = inputTemperature.toFixed(1);
+        convertedTempResult.textContent = `Error! Both temperature scales are same`
     } 
     
     else if (inputScale === 'F' && convertScale === 'F') {
-        convertedTemp = inputTemperature.toFixed(1);
+        convertedTempResult.textContent = `Error! Both temperature scales are same`
     }
-    convertedTempResult.textContent = `Converted Temperature is: ${convertedTemp}`
 }
 
 function resetFields() {
-    document.getElementById('inputTemperature').value = '';
+    document.getElementById('inputTemperature').value = 0;
     document.getElementById('inputScale').selectedIndex = 0;
     document.getElementById('convertScale').selectedIndex = 0;
     document.getElementById('convertedTempResult').textContent = '';
